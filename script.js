d361168d3b17caf57b09285c8ebb1562eb527b7c
IUJOAWDSNAOSDD <106924347+IUJOAWDSNAOSDD@users.noreply.github.com>
2024-06-07 22:21:16 -0700

    added sets of 3 and shortened transcriber

--- script.js
+++ script.js
@@ -1,33 +1,62 @@
+const baseArray = [
+    'A', 'T', 'C', 'G'
+]
+
+
+const baseTranscribedArray = [
+    'U', 'A', 'G', 'C'
+]
+
 function calculate() {
-    let bases = document.getElementById('inputBase').value
-    if (bases == false) {
-        document.getElementById('outputBase').innerHTML = 'Please Enter A Base Line'
+    let base = document.getElementById('inputBase').value
+    if (base == false) {
+        document.getElementById('outputBase').innerHTML = 'Output: Please Enter A Base Line'
     } else {
-        let basesLength = bases.length
+        let baseLength = base.length
 
-    console.log(bases)
 
-    console.log(basesLength)
+        console.log(base)
 
-    const basesTranscribed = []
 
-    for (let i = 0; i < basesLength; i++) {
-        console.log(bases.charAt(i))
-        basesTranscribed[i] = bases.charAt(i)
+        console.log(baseLength)
 
-        if (basesTranscribed[i] === 'A') {
-            basesTranscribed[i] = 'T'
-        } else if (basesTranscribed[i] === 'T') {
-            basesTranscribed[i] = 'A'
-        } else if (basesTranscribed[i] === 'C') {
-            basesTranscribed[i] = 'G'
-        } else {
-            basesTranscribed[i] = 'C'
+
+        const baseTranscribed = []
+
+
+        for (let i = 0; i < baseLength; i++) {
+            baseTranscribed[i] = base.charAt(i)
+            for (let a = 0; a < 4; a++) {
+                if (baseTranscribed[i] === baseArray[a]) {
+                    baseTranscribed[i] = baseTranscribedArray[a]
+                    break
+                }
+            }
         }
-    }
 
-    console.log(basesTranscribed.join(''))
-    
-    document.getElementById('outputBase').innerHTML = 'Output: ' + basesTranscribed.join('')
+
+        console.log(baseTranscribed.join(''))
+        
+        document.getElementById('outputBase').innerHTML = 'Output: ' + baseTranscribed.join('')
+
+
+        console.log(baseTranscribed)
+
+
+        const baseTranscribedLineArray = []
+
+
+        let baseNumber = 0;
+        
+        for (let b = 0; b < baseLength/3; b++) {
+            let baseTemp = []
+            for (c = 0; c < 3; c++) {
+                baseTemp[c] = baseTranscribed[baseNumber]
+                baseNumber += 1
+            }
+            baseTranscribedLineArray[b] = baseTemp.join('')
+        }
+
+        console.log(baseTranscribedLineArray)
     }
 }
